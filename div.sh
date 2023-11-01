4da09d6e55d886585fc3482883c49c05543febc2
scmc24 <manuelsokoudjou@gmail.com>
2023-11-02 00:00:41 +0100

    creation d'un script bash pour executer TestDivision.java apart

---
+++ div.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "\n\n"
+echo "Running Application 3 \n\n"
+echo "=======================================================\n\n"
+
+javac inf3312/concepts/Division.java && 
+javac inf3312/concepts/TestDivision.java && java inf3312/concepts/TestDivision
--- run.sh
+++ run.sh
@@ -9,9 +9,3 @@ echo "=======================================================\n\n"
 javac inf3312/concepts/Point.java && 
 javac inf3312/concepts/TestPoint.java && java inf3312/concepts/TestPoint
 
-echo "\n\n"
-echo "Running Application 3 \n\n"
-echo "=======================================================\n\n"
-
-javac inf3312/concepts/Division.java && 
-javac inf3312/concepts/TestDivision.java && java inf3312/concepts/TestDivision
