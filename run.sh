ef2be7dfd3c8bd320f2442d89bb20740d815f622
scmc24 <manuelsokoudjou@gmail.com>
2023-11-01 23:53:55 +0100

    modification de run.sh pour executer TestDivision.java

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@ javac inf3312/concepts/Voiture.java &&
 javac inf3312/concepts/TestVoiture.java && java inf3312/concepts/TestVoiture
 
 echo "\n\n"
-echo "Running Application 1 \n\n"
+echo "Running Application 1 & 2 \n\n"
 echo "=======================================================\n\n"
 
 javac inf3312/concepts/Point.java && 
 javac inf3312/concepts/TestPoint.java && java inf3312/concepts/TestPoint
+
+echo "\n\n"
+echo "Running Application 3 \n\n"
+echo "=======================================================\n\n"
+
+javac inf3312/concepts/Division.java && 
+javac inf3312/concepts/TestDivision.java && java inf3312/concepts/TestDivision
