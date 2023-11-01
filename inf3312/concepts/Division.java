741009f15e6d9d082590eb71499662d7daba8592
scmc24 <manuelsokoudjou@gmail.com>
2023-11-01 23:52:08 +0100

    Creation des fichiers Division.java et TestDivision.java

---
+++ inf3312/concepts/Division.java
@@ -0,0 +1,17 @@
+
+package inf3312.concepts;
+
+public class Division{
+   
+    public double Diviser(double x , double y){
+     
+     return x/y;
+
+    }
+
+    public static void Diviser(double x , double y){
+        
+        return x/y;
+
+    }
+}
---
+++ inf3312/concepts/TestDivision.java
@@ -0,0 +1,26 @@
+
+package inf3312.concepts;
+
+import java.util.Scanner;
+
+public class TestDivision{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        double x;
+        double y;
+
+        System.out.println("Entrer 2 nombre pour diviser : ");
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+        
+        Division div = new Division();
+        System.out.println("Division par methode de la classe : \n");
+        System.out.println(x+"/"+y+" = "+ div.diviser(x,y) );
+        System.out.println("\n\n");
+        System.out.println("Division par methode de classe : \n");
+        System.out.println(x+"/"+y+" = "+ Division.diviser(x,y) );
+    }
+}
