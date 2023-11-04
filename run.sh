6d7083fa86278600564950089cd8e602f9c0c076
scmc24 <manuelsokoudjou@gmail.com>
2023-11-04 11:21:53 +0100

    modification de run.sh

--- run.sh
+++ run.sh
@@ -9,4 +9,17 @@ echo "=======================================================\n\n"
 javac inf3312/concepts/Point.java && 
 javac inf3312/concepts/Pointcol.java && 
 javac inf3312/concepts/TestPoint.java && java inf3312/concepts/TestPoint
+echo "=======================================================\n\n"
 
+javac inf3312/concepts/Affichable.java &&
+javac inf3312/concepts/Entier.java && 
+javac inf3312/concepts/Flottant.java &&
+javac inf3312/concepts/TestAffichable.java &&
+java inf3312/concepts/TestAffichable
+echo "=======================================================\n\n"
+
+javac inf3312/concepts/Compte.java &&
+javac inf3312/concepts/CompteEpargne.java && 
+javac inf3312/concepts/CompteSecurise.java &&
+javac inf3312/concepts/TestCompte.java &&
+java inf3312/concepts/TestCompte
